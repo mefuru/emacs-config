72b54c7d194b971f5a1c9a16ff226569d9890f95
mefuru <mehul.mandania@gmail.com>
2014-11-07 13:15:51 +0000

    parens

--- init.el
+++ init.el
@@ -160,3 +160,25 @@
 (require 'magit)
 
 (require 'haskell-mode)
+
+;; ---------------------------
+;; -- Paren highlighting --
+;; ---------------------------
+
+;; See matching pairs of parentheses and other characters. When point is on one of the paired characters, the other is highlighted. 
+
+;; Show the matching paren when it is offscreen
+
+(defadvice show-paren-function
+      (after show-matching-paren-offscreen activate)
+      "If the matching paren is offscreen, show the matching line in the
+        echo area. Has no effect if the character before point is not of
+        the syntax class ')'."
+      (interactive)
+      (let* ((cb (char-before (point)))
+             (matching-text (and cb
+                                 (char-equal (char-syntax cb) ?\) )
+                                 (blink-matching-open))))
+        (when matching-text (message matching-text))))
+
+(show-paren-mode 1)
